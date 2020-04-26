ddcddd4df1dea8ab7a4592c679939a34ffdd9754
stone <aa86799@163.com>
2020-04-26 14:43:14 +0800

    MD-theme, button 应用

--- app/src/main/java/com/stone/stoneviewskt/adapter/SampleAdapter3.kt
+++ app/src/main/java/com/stone/stoneviewskt/adapter/SampleAdapter3.kt
@@ -14,11 +14,11 @@ import com.stone.stoneviewskt.R
  * blog :   https://stone.blog.csdn.net
  * time:    2020/4/4 11:14
  */
-class SampleAdapter(private val list: List<String>? = listOf(), private val itemClick: (Int, String)-> Unit): RecyclerView.Adapter<SampleAdapter.ViewHolder>() {
+class SampleAdapter3(private val list: List<String>? = listOf(), private val itemClick: (Int, String)-> Unit): RecyclerView.Adapter<SampleAdapter3.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 //        return ViewHolder(LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false))
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_simple_list1, parent, false))
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_simple_list3, parent, false))
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/stone/stoneviewskt/ui/materialdesign/MDMainFragment.kt
+++ app/src/main/java/com/stone/stoneviewskt/ui/materialdesign/MDMainFragment.kt
@@ -2,7 +2,7 @@ package com.stone.stoneviewskt.ui.materialdesign
 
 import android.os.Bundle
 import com.stone.stoneviewskt.R
-import com.stone.stoneviewskt.adapter.SampleAdapter
+import com.stone.stoneviewskt.adapter.SampleAdapter2
 import com.stone.stoneviewskt.base.BaseActivity
 import com.stone.stoneviewskt.base.BaseFragment
 import com.stone.stoneviewskt.ui.materialdesign.tabv.TabLayoutFragment
@@ -15,14 +15,14 @@ import kotlinx.android.synthetic.main.fragment_md_main.*
  * blog :   https://stone.blog.csdn.net
  * time:    2020/4/25 12:53
  */
-class MDMainFragment: BaseFragment() {
+class MDMainFragment : BaseFragment() {
 
     override fun onPreparedView(savedInstanceState: Bundle?) {
         super.onPreparedView(savedInstanceState)
 
         fragment_md_main_title.mIvLeft.setOnClickListener { mActivity.finish() }
 
-        fragment_md_main_rv.adapter = SampleAdapter(TITLES) { _, title ->
+        fragment_md_main_rv.adapter = SampleAdapter2(TITLES) { _, title ->
             when (title) {
                 "TabLayout+ViewPager" -> (mActivity as BaseActivity).startNewUI(TabLayoutFragment::class.java)
 //                "CoordinatorLayout+AppBarLayout+TabLayout+NestedScrollView" ->
