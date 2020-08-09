d7a448998849fbeaf5f7b0974bdddc39ff38d3ee
stone <aa86799@163.com>
2020-08-09 15:29:07 +0800

    target version = api 30; api30权限申请监听AppOpsManager； api30状态栏设置(待验证)

--- app/src/main/java/com/stone/stoneviewskt/StoneApplication.kt
+++ app/src/main/java/com/stone/stoneviewskt/StoneApplication.kt
@@ -1,7 +1,11 @@
 package com.stone.stoneviewskt
 
 import android.app.Activity
+import android.app.AppOpsManager
+import android.app.AsyncNotedAppOp
+import android.app.SyncNotedAppOp
 import android.content.Context
+import android.os.Build
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.stone.stoneviewskt.base.StoneActivityLifecycleCallbacks
@@ -37,6 +41,32 @@ class StoneApplication : MultiDexApplication() {
         logi("MMKV rootDir: $rootDir")
 //        MMKV.defaultMMKV().encode()
 //        MMKV.defaultMMKV().decodeBool()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {//android 30
+            //当调用需要运行时权限的方法时，会触发回调，应用内只能注册一个，后注册覆盖前一个
+            val appOpsManager = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+
+
+            /*
+             * 分派事件的线程
+             * Context#getMainExecutor()  是 api 28添加的 ，不通用
+             * AsyncTask.THREAD_POOL_EXECUTOR、Executors.newSingleThreadExecutor()
+             */
+            appOpsManager.setOnOpNotedCallback(mainExecutor, object : AppOpsManager.OnOpNotedCallback() {
+                override fun onNoted(snop: SyncNotedAppOp) {//同步
+                    logi("permission: ${snop.op}")
+                }
+
+                override fun onSelfNoted(p0: SyncNotedAppOp) {
+
+                }
+
+                override fun onAsyncNoted(p0: AsyncNotedAppOp) {//异步触发
+
+                }
+
+            })
+        }
     }
 
 }
--- app/src/main/java/com/stone/stoneviewskt/base/BaseActivity.kt
+++ app/src/main/java/com/stone/stoneviewskt/base/BaseActivity.kt
@@ -3,7 +3,9 @@ package com.stone.stoneviewskt.base
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.Window
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.stone.stoneviewskt.R
@@ -24,17 +26,29 @@ open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {//android 30
+//            window.insetsController?.also {
+//                it.hide(WindowInsets.Type.statusBars()) //隐藏状态栏
+//                it.hide(WindowInsets.Type.navigationBars()) //隐藏导航栏
+//            }
+            window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//下面的 View.xx 在 api 30 过时
             //深色字体，明亮状态栏; 默认不设置时，为浅色字，深色状态栏
 //            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+            //浅色字体 深色状态栏
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         }
+
         super.onCreate(savedInstanceState)
+
 //        window.setFlags(//全屏设置后，进入应用首个界面，能看到状态移上去的动作。
 //            WindowManager.LayoutParams.FLAG_FULLSCREEN,
 //            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.statusBarColor = statusBarColor()
-            window.navigationBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT //应用内 导航栏，如 actionBar
         }
 
         (intent?.getSerializableExtra(KEY_FRAGMENT) as? Class<*>)?.let { cls ->
@@ -43,6 +57,7 @@ open class BaseActivity : AppCompatActivity() {
         }
     }
 
+    //状态栏(网络、信号、电量等状态所在栏)颜色
     open fun statusBarColor() = resources.getColor(R.color.colorPrimary)
 
     /*
