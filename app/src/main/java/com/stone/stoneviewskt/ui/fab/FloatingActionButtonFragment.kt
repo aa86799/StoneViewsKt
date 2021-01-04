d073957e2be0d527d57769ac917086f0ece088d7
stone <aa86799@163.com>
2021-01-04 21:43:05 +0800

    ConstraintSet 约束动画

--- app/src/main/java/com/stone/stoneviewskt/ui/fab/FloatingActionButtonFragment.kt
+++ app/src/main/java/com/stone/stoneviewskt/ui/fab/FloatingActionButtonFragment.kt
@@ -1,18 +1,26 @@
 package com.stone.stoneviewskt.ui.fab
 
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.TransitionManager
 import com.stone.stoneviewskt.R
 import com.stone.stoneviewskt.base.BaseFragment
-import kotlinx.android.synthetic.main.fragment_floatingaction_button.*
+import kotlinx.android.synthetic.main.fragment_floatingaction_button.fragment_floatingaction_button_bottom
+import kotlinx.android.synthetic.main.fragment_floatingaction_button.fragment_floatingaction_button_btn
+import kotlinx.android.synthetic.main.fragment_floatingaction_button.fragment_floatingaction_button_top
+import kotlinx.android.synthetic.main.fragment_floatingaction_button_begin.*
 
 /**
- * desc:
+ * desc:    悬浮按钮会处于它所在层次中的前景foreground.
  * author:  stone
  * email:   aa86799@163.com
  * blog :   https://stone.blog.csdn.net
  * time:    2020/5/17 19:12
  */
 class FloatingActionButtonFragment : BaseFragment() {
+
+    private var mIsShowMenu = false
 
     override fun onPreparedView(savedInstanceState: Bundle?) {
         super.onPreparedView(savedInstanceState)
@@ -31,9 +39,29 @@ class FloatingActionButtonFragment : BaseFragment() {
             if (flag) fragment_floatingaction_button_top.extend()
             else fragment_floatingaction_button_top.shrink()
         }
+
+        /*
+         * 约束动画
+         * 要求 约束布局下的每个子view都有 id， 否则报， RuntimeException: All children of ConstraintLayout must have ids to use ConstraintSet.
+         *
+         */
+        val set1 = ConstraintSet()
+        val set2 = ConstraintSet()
+        val root: ConstraintLayout = fragment_fa_button_begin
+        set1.clone(root)
+        set2.clone(requireContext(), R.layout.fragment_floatingaction_button)
+        fragment_floatingaction_button_bottom.setOnClickListener {
+            TransitionManager.beginDelayedTransition(root)
+            mIsShowMenu = !mIsShowMenu
+            if (mIsShowMenu) {
+                set2.applyTo(root)
+            } else {
+                set1.applyTo(root)
+            }
+        }
     }
 
     override fun getLayoutRes(): Int {
-        return R.layout.fragment_floatingaction_button
+        return R.layout.fragment_floatingaction_button_begin
     }
 }
