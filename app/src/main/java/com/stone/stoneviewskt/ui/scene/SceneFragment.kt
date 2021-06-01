f3bed3e3f224494009de4dae1b58bd68bb72be2e
stone <aa86799@163.com>
2021-06-01 23:03:59 +0800

    转场动画(未完成)

---
+++ app/src/main/java/com/stone/stoneviewskt/ui/scene/SceneFragment.kt
@@ -0,0 +1,23 @@
+package com.stone.stoneviewskt.ui.scene
+
+import android.os.Bundle
+import android.view.View
+import com.stone.stoneviewskt.base.BaseFragment
+
+/**
+ * desc:
+ * author:  stone
+ * email:   aa86799@163.com
+ * blog :   https://stone.blog.csdn.net
+ * time:    2021/4/18 18:06
+ */
+class SceneFragment : BaseFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun getLayoutRes(): Int {
+        return super.getLayoutRes()
+    }
+}
