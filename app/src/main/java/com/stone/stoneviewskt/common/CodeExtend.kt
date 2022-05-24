22e7b95931f4fb4fa6c1a3713a12fce0c1c55fd5
stone <aa86799@163.com>
2022-05-24 12:39:15 +0800

    优化debounceClick

--- app/src/main/java/com/stone/stoneviewskt/common/CodeExtend.kt
+++ app/src/main/java/com/stone/stoneviewskt/common/CodeExtend.kt
@@ -1,7 +1,5 @@
 package com.stone.stoneviewskt.common
 
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.coroutineScope
@@ -26,13 +24,13 @@ import java.util.*
  * 会在最后一次 松开超时后， 执行 end
  */
 private val jobUuid = UUID.randomUUID().hashCode()
-private var View.mDebounceSuspendJob: Job?
+private var <T : View> T.mDebounceSuspendJob: Job?
     get() = if (getTag(jobUuid) != null) getTag(jobUuid) as Job? else null
     set(value) {
         setTag(jobUuid, value)
     }
 
-fun View.debounceClick(coroutineScope: CoroutineScope, delayMs: Long = 600L, block: suspend () -> Unit) {
+fun <T : View> T.debounceClick(coroutineScope: CoroutineScope, delayMs: Long = 600L, block: suspend (T) -> Unit) {
     // 对于回调函数 block 中，要考虑 界面销毁，view 为null 的问题。 eg. 异步线程回调、网络请求回调
     setOnClickListener {
         mDebounceSuspendJob?.cancel()
@@ -40,22 +38,22 @@ fun View.debounceClick(coroutineScope: CoroutineScope, delayMs: Long = 600L, blo
             logi("start: ${System.currentTimeMillis()}")
             delay(delayMs)
             logi("end: ${System.currentTimeMillis()}")
-            block()
+            block(this@debounceClick)
             mDebounceSuspendJob = null
         }
     }
 }
 
-fun View.debounceClick(owner: LifecycleOwner, delayMs: Long = 600L, block: suspend () -> Unit) {
+fun <T : View> T.debounceClick(owner: LifecycleOwner, delayMs: Long = 600L, block: suspend (T) -> Unit) {
     debounceClick(owner.lifecycle.coroutineScope, delayMs, block)
 }
 
-fun View.debounceClick(coroutineScope: CoroutineScope, originBlock: View.OnClickListener?, delayMs: Long = 600L) {
+fun <T : View> T.debounceClick(coroutineScope: CoroutineScope, originBlock: View.OnClickListener?, delayMs: Long = 600L) {
     originBlock ?: return
     debounceClick(coroutineScope, delayMs) { originBlock.onClick(this) }
 }
 
-fun View.debounceClick(owner: LifecycleOwner, originBlock: View.OnClickListener?, delayMs: Long = 600L) {
+fun <T : View> T.debounceClick(owner: LifecycleOwner, originBlock: View.OnClickListener?, delayMs: Long = 600L) {
     originBlock ?: return
     debounceClick(owner.lifecycle.coroutineScope, originBlock, delayMs)
 }
@@ -63,33 +61,33 @@ fun View.debounceClick(owner: LifecycleOwner, originBlock: View.OnClickListener?
 /**
  * 利用 Handler + Runnable 防抖
  */
-private val debounceHandle: Handler by lazy { Handler(Looper.getMainLooper()) }
+//private val debounceHandle: Handler by lazy { Handler(Looper.getMainLooper()) }
 
 private val runnableUuid = UUID.randomUUID().hashCode()
-private var View.mDebounceHandleRunnable: Runnable?
+private var <T : View> T.mDebounceHandleRunnable: Runnable?
     get() = if (getTag(runnableUuid) != null) getTag(runnableUuid) as Runnable? else null
     set(value) {
         setTag(runnableUuid, value)
     }
 
-fun View.debounceClickWidthHandler(delayMs: Long = 600L, callback: () -> Unit) {
+fun <T : View> T.debounceClickWidthHandler(delayMs: Long = 600L, callback: (T) -> Unit) {
     // 对于回调函数 callback 中，要考虑 界面销毁，view 为null 的问题。 eg. 异步线程回调、网络请求回调
     setOnClickListener {
-        logi("start: ${System.currentTimeMillis()}  $debounceHandle")
+        logi("start: ${System.currentTimeMillis()}  ${this.handler}")
         mDebounceHandleRunnable?.let {
             loge("remove")
-            debounceHandle.removeCallbacks(it)
+            this.handler.removeCallbacks(it)
         }
         mDebounceHandleRunnable = Runnable {
             logi("end: ${System.currentTimeMillis()}")
-            callback()
+            callback(this)
             mDebounceHandleRunnable = null
         }
-        debounceHandle.postDelayed(mDebounceHandleRunnable!!, delayMs)
+        this.handler.postDelayed(mDebounceHandleRunnable!!, delayMs)
     }
 }
 
-fun View.debounceClickWidthHandler(delayMs: Long = 600L, originBlock: View.OnClickListener?) {
+fun <T : View> T.debounceClickWidthHandler(delayMs: Long = 600L, originBlock: View.OnClickListener?) {
     originBlock ?: return
     debounceClickWidthHandler(delayMs) { originBlock.onClick(this) }
 }
@@ -98,11 +96,27 @@ fun View.debounceClickWidthHandler(delayMs: Long = 600L, originBlock: View.OnCli
  * 记录系统时间，以判断 是否能执行真实点击事件。
  * 会一开始就触发真实回调，后面的连续快速点击，不会触发。
  */
-fun <T : View> T.clickWithTrigger(time: Long = 600, block: (T) -> Unit) {
-    triggerDelay = time
+private val triggerLastTimeUuid = UUID.randomUUID().hashCode()
+private var <T : View> T.triggerLastTime: Long
+    get() = if (getTag(triggerLastTimeUuid) != null) getTag(triggerLastTimeUuid) as Long else 0
+    set(value) {
+        setTag(triggerLastTimeUuid, value)
+    }
+
+private fun <T : View> T.clickEnable(delayMs: Long): Boolean {
+    var flag = false
+    val currentClickTime = System.currentTimeMillis()
+    if (currentClickTime - triggerLastTime >= delayMs) {
+        flag = true
+    }
+    triggerLastTime = currentClickTime
+    return flag
+}
+
+fun <T : View> T.clickWithTrigger(delayMs: Long = 600, block: (T) -> Unit) {
     setOnClickListener {
         logi("start: ${System.currentTimeMillis()}")
-        if (clickEnable()) {
+        if (clickEnable(delayMs)) {
             logi("end: ${System.currentTimeMillis()}")
             block(it as T)
         }
@@ -112,28 +126,4 @@ fun <T : View> T.clickWithTrigger(time: Long = 600, block: (T) -> Unit) {
 fun <T : View> T.clickWithTrigger(originBlock: View.OnClickListener?, time: Long = 600) {
     originBlock ?: return
     clickWithTrigger(time) { originBlock.onClick(this) }
-}
-
-private val triggerLastTimeUuid = UUID.randomUUID().hashCode()
-private var <T : View> T.triggerLastTime: Long
-    get() = if (getTag(triggerLastTimeUuid) != null) getTag(triggerLastTimeUuid) as Long else 0
-    set(value) {
-        setTag(triggerLastTimeUuid, value)
-    }
-
-private val triggerTimeUuid = UUID.randomUUID().hashCode()
-private var <T : View> T.triggerDelay: Long
-    get() = if (getTag(triggerTimeUuid) != null) getTag(triggerTimeUuid) as Long else -1
-    set(value) {
-        setTag(triggerTimeUuid, value)
-    }
-
-private fun <T : View> T.clickEnable(): Boolean {
-    var flag = false
-    val currentClickTime = System.currentTimeMillis()
-    if (currentClickTime - triggerLastTime >= triggerDelay) {
-        flag = true
-    }
-    triggerLastTime = currentClickTime
-    return flag
 }
